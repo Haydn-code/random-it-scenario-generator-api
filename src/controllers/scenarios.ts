dbe9a709170227ac18a857a39ad6948c2fffd6ae
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 20:40:21 +0100

    Outlined a scenarios controller.

---
+++ src/controllers/scenarios.ts
@@ -0,0 +1,5 @@
+import ScenariosService from '../services/scenarios';
+
+export default class ScenariosController extends ScenariosService {
+	protected show(technology: string, role: string, environment: string) {}
+}
