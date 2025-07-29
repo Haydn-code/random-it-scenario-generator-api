3f74cb4d3725eed581eb11c7777cea81a264ab74
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 21:50:54 +0100

    Added tests for scenarios router.

---
+++ src/routers/scenarios.test.ts
@@ -0,0 +1,35 @@
+import ScenariosRouter from './scenarios';
+
+describe('Scenarios Router', () => {
+	it('Fails on invalid method', () => {
+		const testScenariosRouter = new ScenariosRouter();
+		const mockedRes: { writeHead: any; end: any } = { writeHead: jest.fn(), end: jest.fn() };
+		testScenariosRouter.route(
+			'POST',
+			{ technology: 'tech test', role: 'role test', environment: 'environment test' },
+			mockedRes,
+		);
+		expect(mockedRes.writeHead).toHaveBeenCalledWith(405);
+		expect(mockedRes.end).toHaveBeenCalledWith(
+			JSON.stringify({
+				data: {},
+				errors: [
+					{
+						name: '405 error',
+						description: 'This endpoint only supports GET requests',
+					},
+				],
+			}),
+		);
+	});
+	it('Succeeds on GET request', () => {
+		const testScenariosRouter = new ScenariosRouter();
+		const mockedRes: { writeHead: any; end: any } = { writeHead: jest.fn(), end: jest.fn() };
+		testScenariosRouter.route(
+			'POST',
+			{ technology: 'tech test', role: 'role test', environment: 'environment test' },
+			mockedRes,
+		);
+		expect(mockedRes.writeHead).toHaveBeenCalledWith(200);
+	});
+});
