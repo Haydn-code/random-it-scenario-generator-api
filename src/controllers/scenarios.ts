5975de24736a38dca89859cb75e2d34788d74e33
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 20:58:48 +0100

    Added an expected return type from the scenario controller.

--- src/controllers/scenarios.ts
+++ src/controllers/scenarios.ts
@@ -1,5 +1,21 @@
 import ScenariosService from '../services/scenarios';
 
 export default class ScenariosController extends ScenariosService {
-	protected show(technology: string, role: string, environment: string) {}
+	protected show(
+		technology: string,
+		role: string,
+		environment: string,
+	): {
+		data: {
+			inputs: { technology: string; role: string; environment: string };
+			scenario: { challenge: string; incident: string; 'troubleshooting step': string };
+		};
+	} {
+		return {
+			data: {
+				inputs: { technology, role, environment },
+				scenario: { challenge: '', incident: '', 'troubleshooting step': '' },
+			},
+		};
+	}
 }
