3316dc1f084bc4d6b2a20e553fb1b7980d618ceb
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 22:46:06 +0100

    Fixed an issue, where context wasn't being preserved in the server.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import MainRouter from './routers/main';
 
 const port: number = 8080;
 const mainRouter = new MainRouter();
-const server = http.createServer(mainRouter.route);
+const server = http.createServer((req, res) => mainRouter.route(req, res));
 server.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
