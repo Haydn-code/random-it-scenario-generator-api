da03f94dab464b62189fb0ddb73ee6193b6e6a3b
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 22:01:41 +0100

    Added testing and implementation for scenarios router.

--- src/routers/scenarios.test.ts
+++ src/routers/scenarios.test.ts
@@ -3,30 +3,36 @@ import ScenariosRouter from './scenarios';
 describe('Scenarios Router', () => {
 	it('Fails on invalid method', () => {
 		const testScenariosRouter = new ScenariosRouter();
-		const mockedRes: { writeHead: any; end: any } = { writeHead: jest.fn(), end: jest.fn() };
+		const mockedRes: { setHeader: any; writeHead: any; end: any } = {
+			setHeader: jest.fn(),
+			writeHead: jest.fn(),
+			end: jest.fn(),
+		};
 		testScenariosRouter.route(
 			'POST',
 			{ technology: 'tech test', role: 'role test', environment: 'environment test' },
 			mockedRes,
 		);
 		expect(mockedRes.writeHead).toHaveBeenCalledWith(405);
-		expect(mockedRes.end).toHaveBeenCalledWith(
-			JSON.stringify({
-				data: {},
-				errors: [
-					{
-						name: '405 error',
-						description: 'This endpoint only supports GET requests',
-					},
-				],
-			}),
-		);
+		expect(mockedRes.end).toHaveBeenCalledWith({
+			data: {},
+			errors: [
+				{
+					name: '405 error',
+					description: 'This endpoint only supports GET requests',
+				},
+			],
+		});
 	});
 	it('Succeeds on GET request', () => {
 		const testScenariosRouter = new ScenariosRouter();
-		const mockedRes: { writeHead: any; end: any } = { writeHead: jest.fn(), end: jest.fn() };
+		const mockedRes: { setHeader: any; writeHead: any; end: any } = {
+			setHeader: jest.fn(),
+			writeHead: jest.fn(),
+			end: jest.fn(),
+		};
 		testScenariosRouter.route(
-			'POST',
+			'GET',
 			{ technology: 'tech test', role: 'role test', environment: 'environment test' },
 			mockedRes,
 		);
--- src/routers/scenarios.ts
+++ src/routers/scenarios.ts
@@ -1,3 +1,27 @@
-export default class ScenariosRouter {
-	public route(method: string, queries: { [key: string]: string }, res: any) {}
+import ScenariosController from '../controllers/scenarios';
+
+export default class ScenariosRouter extends ScenariosController {
+	public route(method: string, queries: { [key: string]: string }, res: any) {
+		res.setHeader('Content-Type', 'application/json');
+		switch (method) {
+			case 'GET':
+				res.writeHead(200);
+				const technology = 'technology' in queries ? queries['technology'] : '';
+				const role = 'role' in queries ? queries['role'] : '';
+				const environment = 'environment' in queries ? queries['environment'] : '';
+				res.end(this.show(technology, role, environment));
+				break;
+			default:
+				res.writeHead(405);
+				res.end({
+					data: {},
+					errors: [
+						{
+							name: '405 error',
+							description: 'This endpoint only supports GET requests',
+						},
+					],
+				});
+		}
+	}
 }
