aacf2b311f86f2db9fdfee558f63341e17694dd7
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 19:34:02 +0100

    Added testing for ScenariosService.

--- src/services/scenarios.test.ts
+++ src/services/scenarios.test.ts
@@ -1,7 +1,29 @@
 import ScenariosService from './scenarios';
+import Scenarios from '../fixtures/scenarios.json';
 
 class TestScenariosService extends ScenariosService {
-	public randomScenario(technology: string, role: string, environment: string): string {
+	public randomScenario(technology: string, role: string, environment: string) {
 		return super.randomScenario(technology, role, environment);
 	}
 }
+
+describe('Scenarios Service', () => {
+	const testScenariosService = new TestScenariosService();
+
+	it('Returns challenges, incidents and troubleshooting steps', () => {
+		expect(testScenariosService.randomScenario('', '', '')).toBeInstanceOf({
+			challenges: [],
+			incidents: [],
+			'troubleshooting steps': [],
+		});
+	});
+	it('Returns an object with the specified parameters', () => {
+		expect(Scenarios['Cloud Computing']['Software Engineer']['On-Prem Data Center']).toContain(
+			testScenariosService.randomScenario(
+				'Cloud Computing',
+				'Software Engineer',
+				'On-Prem Data Center',
+			),
+		);
+	});
+});
