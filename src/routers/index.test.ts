0f4586496c4e61c06c1c2bde3da84fa6a8e211b5
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 22:08:27 +0100

    Added index router functionality.

---
+++ src/routers/index.test.ts
@@ -0,0 +1,33 @@
+import IndexRouter from './index';
+
+describe('Index Router', () => {
+	it('Fails on invalid method', () => {
+		const testIndexRouter = new IndexRouter();
+		const mockedRes: { setHeader: any; writeHead: any; end: any } = {
+			setHeader: jest.fn(),
+			writeHead: jest.fn(),
+			end: jest.fn(),
+		};
+		testIndexRouter.route('POST', {}, mockedRes);
+		expect(mockedRes.writeHead).toHaveBeenCalledWith(405);
+		expect(mockedRes.end).toHaveBeenCalledWith({
+			data: {},
+			errors: [
+				{
+					name: '405 error',
+					description: 'This endpoint only supports GET requests',
+				},
+			],
+		});
+	});
+	it('Succeeds on GET request', () => {
+		const testIndexRouter = new IndexRouter();
+		const mockedRes: { setHeader: any; writeHead: any; end: any } = {
+			setHeader: jest.fn(),
+			writeHead: jest.fn(),
+			end: jest.fn(),
+		};
+		testIndexRouter.route('GET', {}, mockedRes);
+		expect(mockedRes.writeHead).toHaveBeenCalledWith(200);
+	});
+});
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,3 +1,23 @@
-export default class IndexRouter {
-	public route(method: string, queries: { [key: string]: string }, res: any) {}
+import IndexController from '../controllers';
+
+export default class IndexRouter extends IndexController {
+	public route(method: string, queries: { [key: string]: string }, res: any) {
+		res.setHeader('Content-Type', 'application/json');
+		switch (method) {
+			case 'GET':
+				res.writeHead(200);
+				res.end(this.show);
+			default:
+				res.writeHead(405);
+				res.end({
+					data: {},
+					errors: [
+						{
+							name: '405 error',
+							description: 'This endpoint only supports GET requests',
+						},
+					],
+				});
+		}
+	}
 }
