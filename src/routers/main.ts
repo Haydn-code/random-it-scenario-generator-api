9f6ff746049b5e1d23d9249d81c675c52a6ae653
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 22:32:39 +0100

    Implemented functionality for main router.

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,3 +1,47 @@
+import IndexRouter from './index';
+import ScenariosRouter from './scenarios';
+
 export default class MainRouter {
-	public route(req: any, res: any) {}
+	protected indexRouter = new IndexRouter();
+	protected scenariosRouter = new ScenariosRouter();
+	public route(req: any, res: any) {
+		const urlQuery = req.url.split('?');
+		const url = urlQuery[0];
+		const queries = urlQuery.length > 1 ? this.parseQueries(urlQuery[1]) : {};
+		switch (url) {
+			case '/':
+				this.indexRouter.route(req.method, queries, res);
+				break;
+			case '/scenarios':
+				this.scenariosRouter.route(req.method, queries, res);
+				break;
+			default:
+				res.setHeader('Content-Type', 'application/json');
+				res.writeHead(404);
+				res.end({
+					data: {},
+					errors: [
+						{
+							name: '404 error',
+							description: "This page doesn't exist",
+						},
+					],
+				});
+		}
+	}
+	public parseQueries(queries: string): { [key: string]: string } {
+		const identifiedQueries: { [key: string]: string } = {};
+
+		// Gets each query parameter.
+		const splitQueries = queries.split('&');
+
+		// Loops through and appends each valid query parameter.
+		for (const query of splitQueries) {
+			const queryToValue = query.split('=');
+			if (queryToValue.length == 2) {
+				identifiedQueries[queryToValue[0]] = queryToValue[1];
+			}
+		}
+		return identifiedQueries;
+	}
 }
