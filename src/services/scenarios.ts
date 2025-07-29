3b0e89417b0da4c9a21547342e818e38a8d2e3f0
Haydn-code <haydnsmall58@gmail.com>
2025-07-29 21:13:20 +0100

    Added TSDocs for scenario service.

--- src/services/scenarios.ts
+++ src/services/scenarios.ts
@@ -1,6 +1,14 @@
 import { scenarios } from '../fixtures/scenarios';
 
 export default class ScenariosService {
+	/**
+	 * Generates a random scenario given technology, role and environment.
+	 *
+	 * @param technology - The technology for the scenario to be centered around.
+	 * @param role - The role for the scenario to be centered around.
+	 * @param environment - The environment for the scenario to be centered around.
+	 * @returns - The generated scenario.
+	 */
 	protected randomScenario(technology: string, role: string, environment: string) {
 		// Gets technology
 		if (!(technology in scenarios)) {
